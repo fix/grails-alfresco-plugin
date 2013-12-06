d5d45c1e42d791fc0c37b9c7c281321375339e96
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2013-12-06 15:59:00 +0800

    Upgraded to Grails 2.3.3, compiles and runs

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,16 +16,18 @@ grails.project.dependency.resolution = {
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
+        // mavenLocal()
+        mavenCentral()
+        // mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
+        build "org.grails.plugins:tomcat:7.0.47"
+        runtime "org.grails.plugins:hibernate:3.6.10.4" // or ":hibernate4:4.1.11.4"
+        
         // runtime 'mysql:mysql-connector-java:5.1.13'
     }
 }
